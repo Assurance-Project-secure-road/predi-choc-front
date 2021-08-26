02e80c83ececaaf241dec004e9abe9b25fdfcef2
simplg <81249731+simplg@users.noreply.github.com>
2021-08-26 15:34:59 +0200

    Better skeleton for vue app
    Home is now the main view and components should be added to it
    
    Each component can take as an example StepExemple.vue
    
    Bootstrap is now integrated by default
    
    Logo is now changed to Predichoc one

---
+++ src/components/Result.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Bravo !</h1>
+<h3>Vos résultats</h3>
+<ul>
+    <li v-for="(result, index) in results" :key="index">{{ result.score }}</li>
+</ul>
+</template>
+
+<script setup>
+import { defineProps } from "vue"
+defineProps({
+    results: Object,
+})
+</script>
